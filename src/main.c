1713939e184e290d717d258cc352ff969f11986f
nicolasgsantana <nicolas07san@gmail.com>
2025-10-13 16:02:29 -0300

    test valid_input

--- src/main.c
+++ src/main.c
@@ -6,15 +6,24 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:04:14 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/09 16:06:59 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/13 15:58:54 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int	i;
+
+	if (argc > 1)
+	{
+		i = 1;
+		while (argv[i])
+		{
+			ft_printf("Arg: %s isvalid: %d\n", argv[i], is_input_valid(argv[i]));
+			i++;
+		}
+	}
 	return (0);
 }
