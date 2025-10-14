5512278da3298b33c31af77b3c35acf3aa413537
nicolasgsantana <nicolas07san@gmail.com>
2025-10-14 15:55:58 -0300

    concentrate validation functions

--- src/validate.c
+++ src/validate.c
@@ -6,23 +6,18 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 15:50:46 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/13 15:56:35 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/14 15:49:29 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	ft_issign(int c)
-{
-	return (c == '+' || c == '-');
-}
-
 int	is_number_valid(long n)
 {
-	return (!(n > INT_MAX || n < INT_MIN));
+	return (n <= (long)INT_MAX && n >= (long)INT_MIN);
 }
 
-int	is_input_valid(char *str)
+int	is_string_valid(char *str)
 {
 	int	i;
 
@@ -47,4 +42,65 @@ int	is_input_valid(char *str)
 		i++;
 	}
 	return (1);
+}
+
+int	is_there_a_duplicate(char **array)
+{
+	int		i;
+	int		j;
+	size_t	len_i;
+
+	i = 0;
+	j = 0;
+	while (array[i])
+	{
+		len_i = ft_strlen(array[i]);
+		j = i + 1;
+		while (array[j])
+		{
+			if (len_i == ft_strlen(array[j]))
+				if (!ft_strncmp(array[i], array[j], len_i))
+					return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	free_matrix_return(char **matrix, int argc, int return_value)
+{
+	int	i;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (matrix[i])
+			free(matrix[i++]);
+		free(matrix);
+	}
+	return (return_value);
+}
+
+int	is_input_valid(int argc, char **argv)
+{
+	int		i;
+	char	**inputs;
+
+	if (argc == 2)
+		inputs = ft_split(argv[1], ' ');
+	else if (argc > 2)
+		inputs = ++argv;
+	i = 0;
+	while (inputs[i])
+	{
+		if (!is_string_valid(inputs[i]))
+			return (free_matrix_return(inputs, argc, 0));
+		else if (!is_number_valid(ft_atol(inputs[i])))
+			return (free_matrix_return(inputs, argc, 0));
+		i++;
+	}
+	if (is_there_a_duplicate(inputs))
+		return (free_matrix_return(inputs, argc, 0));
+	return (free_matrix_return(inputs, argc, 1));
 }
