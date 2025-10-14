3f24c1cbe7b1bef9d60a7a29b0e6fafe85282b0c
nicolasgsantana <nicolas07san@gmail.com>
2025-10-14 15:58:41 -0300

    add test to check input validation

--- src/main.c
+++ src/main.c
@@ -6,24 +6,25 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:04:14 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/13 15:58:54 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/14 15:46:51 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	error(void)
+{
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	return (1);
+}
+
 int	main(int argc, char **argv)
 {
-	int	i;
-
-	if (argc > 1)
+	if (argc >= 2)
 	{
-		i = 1;
-		while (argv[i])
-		{
-			ft_printf("Arg: %s isvalid: %d\n", argv[i], is_input_valid(argv[i]));
-			i++;
-		}
+		if (!is_input_valid(argc, argv))
+			return (error());
+		ft_printf("Input is valid\n");
 	}
 	return (0);
 }
