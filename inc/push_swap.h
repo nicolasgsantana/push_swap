bc9804493e67c14f97147a741cc5d4af782cfe79
nicolasgsantana <nicolas07san@gmail.com>
2025-10-14 15:57:46 -0300

    add validation functions

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:03:19 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/13 15:59:30 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/14 15:01:49 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 
 long	ft_atol(const char *nptr);
 int		is_number_valid(long n);
-int		is_input_valid(char *str);
+int		ft_issign(int c);
+int		is_string_valid(char *str);
+int		is_there_a_duplicate(char **array);
+int		is_input_valid(int argc, char **argv);
 
 #endif
