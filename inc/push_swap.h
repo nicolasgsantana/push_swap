7520b8b98641dfebb0628bef1f9d62f195f4342f
nicolasgsantana <nicolas07san@gmail.com>
2025-10-15 13:56:56 -0300

    add rotate operations

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 16:03:19 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/15 13:17:39 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/15 13:33:22 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,8 @@ void	pb(t_list **stack_a, t_list **stack_b);
 void	sa(t_list **stack_a);
 void	sb(t_list **stack_b);
 void	ss(t_list **stack_a, t_list **stack_b);
+void	ra(t_list **stack_a);
+void	rb(t_list **stack_b);
+void	rr(t_list **stack_a, t_list **stack_b);
 
 #endif
---
+++ src/rotate_op.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_op.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 13:23:23 by nde-sant          #+#    #+#             */
+/*   Updated: 2025/10/15 13:54:40 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rotate(t_list **stack)
+{
+	t_list	*last_node;
+	t_list	*first_node;
+
+	if (ft_lstsize(*stack) < 2)
+		return ;
+	first_node = *stack;
+	*stack = (*stack) -> next;
+	first_node -> next = NULL;
+	last_node = *stack;
+	while(last_node -> next)
+		last_node = last_node -> next;
+	last_node -> next = first_node;
+}
+
+void	ra(t_list **stack_a)
+{
+	rotate(stack_a);
+	ft_printf("ra\n");
+}
+
+void	rb(t_list **stack_b)
+{
+	rotate(stack_b);
+	ft_printf("rb\n");
+}
+
+void	rr(t_list **stack_a, t_list **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_printf("rr\n");
+}
