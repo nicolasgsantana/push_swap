985857ac645e2e5856129ceeefc41a6fd0625feb
nicolasgsantana <nicolas07san@gmail.com>
2025-10-20 11:23:41 -0300

    update to keep arrow operator concise

--- src/debug.c
+++ src/debug.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 10:35:29 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/20 10:38:03 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/20 11:22:47 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,13 @@ void	print_stacks(t_list *stack_a, t_list *stack_b)
 	{
 		if (tmp_a)
 		{
-			nb_a = *(int *)tmp_a -> content;
-			tmp_a = tmp_a -> next;
+			nb_a = *(int *)tmp_a->content;
+			tmp_a = tmp_a->next;
 		}
 		if (tmp_b)
 		{
-			nb_b = *(int *)tmp_b -> content;
-			tmp_b = tmp_b -> next;
+			nb_b = *(int *)tmp_b->content;
+			tmp_b = tmp_b->next;
 		}
 		ft_printf("%d  %d\n", nb_a, nb_b);
 	}
