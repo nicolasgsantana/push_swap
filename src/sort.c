114459e53a023da6de332860dccf548f5a7f2e8b
nicolasgsantana <nicolas07san@gmail.com>
2025-10-22 17:17:49 -0300

    add sort_big

--- src/sort.c
+++ src/sort.c
@@ -6,11 +6,21 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 15:28:26 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/20 16:22:11 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/22 16:35:32 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	sort_big(t_list **stack_a, t_list **stack_b)
+{
+	while (ft_lstsize(*stack_a) > 3)
+	{
+		if (ft_lstsize(*stack_b) < 2)
+			pb(stack_a, stack_b);
+	}
+	sort_three(stack_a);
+}
 
 void	sort(t_list **stack_a, t_list **stack_b)
 {
@@ -19,5 +29,5 @@ void	sort(t_list **stack_a, t_list **stack_b)
 	else if (ft_lstsize(*stack_a) == 3)
 		sort_three(stack_a);
 	else
-		(void)stack_b;	
+		sort_big(stack_a, stack_b);
 }
