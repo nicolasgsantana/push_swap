4f9c79ff24fa8d76e63ca73cc6564f6eb68e2fc2
nicolasgsantana <nicolas07san@gmail.com>
2025-10-24 17:28:52 -0300

    remove ft_abs_diff

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 15:01:16 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/22 16:24:38 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/24 14:36:22 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,4 @@ int	ft_abs(int n)
 	if (n < 0)
 		return (-n);
 	return (n);
-}
-
-int	ft_abs_diff(int n1, int n2)
-{
-	int	abs_n1;
-	int	abs_n2;
-
-	abs_n1 = ft_abs(n1);
-	abs_n2 = ft_abs(n2);
-	return (ft_abs(abs_n1 - abs_n2));
 }
