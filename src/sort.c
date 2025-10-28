739348637c3fa7cd8b42e24ff134a3afd7ad20dc
nicolasgsantana <nicolas07san@gmail.com>
2025-10-28 11:40:54 -0300

    fix error with cost

--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 15:28:26 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/27 11:54:39 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/10/28 11:22:04 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,37 @@ void	rot_stacks(t_list **stack_a, t_list **stack_b, int cost_a, int cost_b)
 	while (cost_a != 0 || cost_b != 0)
 	{
 		if (cost_a > 0 && cost_b > 0)
+		{
 			rr(stack_a, stack_b);
-		else if (cost_a < 0 && cost_b < 0)
-			rrr(stack_a, stack_b);
-		else if (cost_a > 0)
-			ra(stack_a);
-		else if (cost_a < 0)
-			rra(stack_a);
-		else if (cost_b > 0)
-			rb(stack_b);
-		else if (cost_b < 0)
-			rrb(stack_b);
-		if (cost_a > 0)
 			cost_a--;
-		else if (cost_a < 0)
-			cost_a++;
-		if (cost_b > 0)
 			cost_b--;
-		else if (cost_b < 0)
+		}
+		else if (cost_a < 0 && cost_b < 0)
+		{
+			rrr(stack_a, stack_b);
+			cost_a++;
 			cost_b++;
+		}
+		else if (cost_a > 0)
+		{
+			ra(stack_a);
+			cost_a--;
+		}
+		else if (cost_a < 0)
+		{
+			rra(stack_a);
+			cost_a++;
+		}
+		else if (cost_b > 0)
+		{
+			rb(stack_b);
+			cost_b--;
+		}
+		else if (cost_b < 0)
+		{
+			rrb(stack_b);
+			cost_b++;
+		}
 	}
 }
 
