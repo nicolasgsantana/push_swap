56b6d38c24e7f33d6e0c1b97111f1b709b808e5d
nicolasgsantana <nicolas07san@gmail.com>
2025-11-05 13:12:41 -0300

    add check for empty matrix before freeing

--- src/free.c
+++ src/free.c
@@ -6,7 +6,7 @@
 /*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 14:02:49 by nde-sant          #+#    #+#             */
-/*   Updated: 2025/10/30 14:43:28 by nde-sant         ###   ########.fr       */
+/*   Updated: 2025/11/05 10:52:38 by nde-sant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,10 @@ void	free_matrix(char **matrix)
 	int	i;
 
 	i = 0;
-	while (matrix[i])
-		free(matrix[i++]);
-	free(matrix);
+	if (matrix)
+	{
+		while (matrix[i])
+			free(matrix[i++]);
+		free(matrix);
+	}
 }
